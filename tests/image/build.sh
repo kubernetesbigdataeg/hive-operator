1807a16dc39cae1dc20fbcebee5c0da3aadaec85
MarioLopezz <mlopezzbueno@gmail.com>
2023-01-13 10:58:48 +0100

    cambios provisionales

--- tests/image/build.sh
+++ tests/image/build.sh
@@ -1,3 +1,3 @@
-podman build . --tag docker.io/kubernetesbigdataeg/hive-metastore:3.0.0-1
+podman build . --tag docker.io/kubernetesbigdataeg/hive-metastore:3.1.2-1
 podman login docker.io
-podman push docker.io/kubernetesbigdataeg/hive-metastore:3.0.0-1
+podman push docker.io/kubernetesbigdataeg/hive-metastore:3.1.2-1
--- tests/image/entrypoint.sh
+++ tests/image/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 source /etc/environments/hive.env
-/propgen -label HIVE -render metastoresite -file /opt/hive-metastore/conf/metastore-site.xml
+/propgen -label HIVE -render hivesite -file /opt/hive/conf/hive-site.xml
 
-exec "/opt/hive-metastore/bin/start-metastore"
+exec /opt/hive/bin/hive --service metastore --hiveconf hive.root.logger=DEBUG,console
